58111ea0f1db374e5e096570e2afb748967f3ac4
jesus.martinezimi <jesus.martinezimi@gmail.com>
2026-05-14 20:42:09 -0500

    app modify

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import plotly.graph_objects as go  # Importación de plotly.graph_objects como go
+import plotly.express as px
 import streamlit as st
 
 # Leer los datos del archivo CSV
@@ -14,18 +14,14 @@ if hist_button:
     st.write(
         'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
 
-    # Crear un histograma utilizando plotly.graph_objects
-    # Se crea una figura vacía y luego se añade un rastro de histograma
-    fig = go.Figure(data=[go.Histogram(x=car_data['odometer'])])
-
-    # Opcional: Puedes añadir un título al gráfico si lo deseas
-    fig.update_layout(title_text='Distribución del Odómetro')
+    # Crear un histograma utilizando plotly.express
+    fig = px.histogram(car_data, x='odometer',
+                       title='Distribución del Odómetro')
 
     # Mostrar el gráfico Plotly interactivo en la aplicación Streamlit
-    # 'use_container_width=True' ajusta el ancho del gráfico al contenedor
     st.plotly_chart(fig, use_container_width=True)
 
-    # Crear un botón en la aplicación Streamlit
+# Crear un botón en la aplicación Streamlit
 scatter_button = st.button('Construir Disperción')
 
 # Lógica a ejecutar cuando se hace clic en el botón
@@ -33,12 +29,9 @@ if scatter_button:
     # Escribir un mensaje en la aplicación
     st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
 
-    # Crear un gráfico de dispersión utilizando plotly.graph_objects
-    fig = go.Figure(data=go.Scatter(
-        x=car_data['odometer'], y=car_data['price'], mode='markers'))
-
-    # Opcional: Puedes añadir un título al gráfico si lo deseas
-    fig.update_layout(title_text='Dispersión del Odómetro vs Precio')
+    # Crear un gráfico de dispersión utilizando plotly.express
+    fig = px.scatter(car_data, x='odometer', y='price',
+                     title='Dispersión del Odómetro vs Precio')
 
     # Mostrar el gráfico Plotly interactivo en la aplicación Streamlit
     st.plotly_chart(fig, use_container_width=True)
